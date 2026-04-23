21827b6cdabfb373224d9a7230758bff6dd18665
jecem ben slama <benslemajecem@gmail.com>
2026-04-23 01:00:16 +0100

    updated env and angular.json for  CI/CD deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: false,
-  // The base URL for all your Spring Boot endpoints [cite: 4]
-  apiUrl: 'http://localhost:8080/api' 
+  production: true,
+  apiUrl: 'https://projectcollaborationapi-production.up.railway.app/api',
 };
